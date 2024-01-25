94e18debbe14c256eaa2fb36508f2ccec9b5492f
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-01-25 11:43:58 +0700

    edit on login page -- testing firebase auth

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -1,5 +1,7 @@
-import 'package:flix_id/data/dummies/dummy_authentication.dart';
-import 'package:flix_id/data/dummies/dummy_user_repository.dart';
+// import 'package:flix_id/data/dummies/dummy_authentication.dart';
+// import 'package:flix_id/data/dummies/dummy_user_repository.dart';
+import 'package:flix_id/data/firebase/firebase_authentication.dart';
+import 'package:flix_id/data/firebase/firebase_repository.dart';
 import 'package:flix_id/domain/usecases/login/login.dart';
 import 'package:flix_id/presentation/pages/main_page/main_page.dart';
 import 'package:flutter/material.dart';
@@ -16,8 +18,8 @@ const LoginPage({ Key? key }) : super(key: key);
       body: Center(
         child: ElevatedButton(onPressed: (){
           Login login = Login(
-            authentication: DummyAuthentication(),
-            userRepository: DummyUserRepository());
+            authentication: FirebaseAuthentication(),
+            userRepository: FirebaseUserRepository());
 
             login(LoginParams(email: 'email', password: 'password'))
             .then((result) {
