cfd6fe9125a17c8b8cda2ef7f0f4d6bb70081d3b
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-01-25 21:23:48 +0700

    riverpod - implementation on presentation layer - riverpod provider

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -4,23 +4,23 @@ import 'package:flix_id/data/firebase/firebase_authentication.dart';
 import 'package:flix_id/data/firebase/firebase_user_repository.dart';
 import 'package:flix_id/domain/usecases/login/login.dart';
 import 'package:flix_id/presentation/pages/main_page/main_page.dart';
+import 'package:flix_id/presentation/providers/usecases/login_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerWidget {
 const LoginPage({ Key? key }) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context, WidgetRef ref){
     return Scaffold(
       appBar: AppBar(
         title: const Text('Main Page')
         ),
       body: Center(
         child: ElevatedButton(onPressed: (){
-          Login login = Login(
-            authentication: FirebaseAuthentication(),
-            userRepository: FirebaseUserRepository());
-
+          Login login = ref.watch(loginProvider);
+          
             login(LoginParams(email: 'faishalyb@gmail.com', password: '123123'))
             .then((result) {
               if (result.isSuccess) {
--- lib/presentation/providers/repositories/authentication/authentication_provider.dart
+++ lib/presentation/providers/repositories/authentication/authentication_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flix_id/data/dummies/dummy_authentication.dart';
 import 'package:flix_id/data/firebase/firebase_authentication.dart';
 import 'package:flix_id/data/repositories/authentication.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
