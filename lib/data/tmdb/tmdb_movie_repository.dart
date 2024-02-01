d315616ff280f8662affed5bdb11025ed0adaea0
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-02-01 10:05:49 +0700

    add dio packages & tmdb movie repository -- nowplaying % upcoming

---
+++ lib/data/tmdb/tmdb_movie_repository.dart
@@ -0,0 +1,68 @@
+import 'package:dio/dio.dart';
+import 'package:flix_id/data/repositories/movie_repository.dart';
+import 'package:flix_id/domain/entities/actor.dart';
+import 'package:flix_id/domain/entities/movie.dart';
+import 'package:flix_id/domain/entities/movie_detail.dart';
+import 'package:flix_id/domain/entities/result.dart';
+
+class TmdbMovieRepository implements MovieRepository {
+  final Dio? _dio;
+  final String _accessToken = 
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTk3YTViNjM2NjUzMTMzY2RmMjJhNzhmYjc2MmRkYSIsInN1YiI6IjY1OWJkNDJlY2E0ZjY3MDFmZTc3NzhiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PHHdDcxzhtJeyFXc2jpfHPtUscWSZ9LNAJtg-gqWclI';
+
+  late final Options _options = Options(headers:  {
+    'Authorization': 'Bearer$_accessToken',
+    'accept': 'application/json'
+  });
+
+  TmdbMovieRepository({Dio? dio}) : _dio = dio ?? Dio();
+   
+  @override
+  Future<Result<List<Actor>>> getActors({required int id}) {
+    // TODO: implement getActors
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<MovieDetail>> getDetail({required int id}) {
+    // TODO: implement getDetail
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<List<Movie>>> getNowPlaying({int page = 1}) async =>
+    _getMovies(_MovieCategory.nowPlaying.toString(), page: page);
+
+  @override
+  Future<Result<List<Movie>>> getUpcoming({int page = 1}) async =>
+    _getMovies(_MovieCategory.upcoming.toString(), page: page);
+
+  Future<Result<List<Movie>>> _getMovies(String category,{int page = 1}) async {
+  try {
+    final response = await _dio!.get(
+      'https://api.themoviedb.org/3/movie/$category/?language=en-US&page=$page',
+      options: _options);
+
+    final result = List<Map<String, dynamic>>.from(response.data['results']);
+
+    return Result.success(
+      result.map((e)=> Movie.fromJSON(e)).toList());
+  }
+  on DioException catch (e) {
+    return Result.failed('${e.message}');
+    }
+  }
+}
+
+
+enum _MovieCategory {
+  nowPlaying('now_playing'),
+  upcoming('upcoming');
+
+  final String _instring;
+
+  const _MovieCategory(String inString): _instring = inString;
+
+  @override
+  String toString() => _instring;
+}
