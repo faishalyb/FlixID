62b57fcf18b0291775d2e171b2827202a5bbe29f
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-02-07 22:19:03 +0700

    user data provider - getloggedinuser

---
+++ lib/presentation/providers/user_data/user_data_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flix_id/domain/entities/result.dart';
+import 'package:flix_id/domain/entities/user.dart';
+import 'package:flix_id/domain/usecases/get_logged_in_user/get_logged_in_user.dart';
+import 'package:flix_id/presentation/providers/movie/now_playing_provider.dart';
+import 'package:flix_id/presentation/providers/movie/upcoming_provider.dart';
+import 'package:flix_id/presentation/providers/usecases/get_logged_in_user_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_data_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+class UserData extends _$UserData {
+  @override
+  Future<User?> build() async {
+    GetLoggedInUser getLoggedInUser = ref.read(getLoggedInUserProvider);
+    var userResult = await getLoggedInUser(null);
+
+    switch (userResult) {
+      case Success(value: final user):
+        _getMovies();
+        return user;
+      case Failed(message: _):
+        return null;
+    }
+  }
+
+  void _getMovies() {
+    ref.read(nowPlayingProvider.notifier).getMovies();
+    ref.read(upcomingProvider.notifier).getMovies();
+  }
+}
