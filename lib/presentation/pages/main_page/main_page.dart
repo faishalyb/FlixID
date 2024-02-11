420d7280288372c679d06df4df55ba8aa22bd4b7
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-02-11 15:13:53 +0700

    remove unused on mainpage, adding user data provider on text

--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,11 +1,9 @@
-import 'package:flix_id/domain/entities/user.dart';
 import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MainPage extends ConsumerStatefulWidget {
-  final User user;
-  const MainPage({Key? key, required this.user}) : super(key: key);
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   ConsumerState<MainPage> createState() => _MainPageState();
