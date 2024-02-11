778e24cdb50fdc2219e51f5517685a9c7630fc07
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-02-11 15:37:43 +0700

    adding logout elevated button

--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flix_id/presentation/providers/router/router_provider.dart';
 import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,13 +13,27 @@ class MainPage extends ConsumerStatefulWidget {
 class _MainPageState extends ConsumerState<MainPage> {
   @override
   Widget build(BuildContext context) {
+    ref.listen(userDataProvider, (previous, next) {
+      if (previous != null && next is AsyncData && next.value == null) {
+        ref.read(routerProvider).goNamed('login');
+      }
+    });
     return Scaffold(
       appBar: AppBar(title: const Text('Main Page')),
       body: Center(
-        child: Text(ref.watch(userDataProvider).when(
-            data: (data) => data.toString(),
-            error: (error, StackTrace) => '',
-            loading: () => 'Loading')),
+        child: Column(
+          children: [
+            Text(ref.watch(userDataProvider).when(
+                data: (data) => data.toString(),
+                error: (error, StackTrace) => '',
+                loading: () => 'Loading')),
+            ElevatedButton(
+                onPressed: () {
+                  ref.read(userDataProvider.notifier).logout();
+                },
+                child: Text('Logout'))
+          ],
+        ),
       ),
     );
   }
