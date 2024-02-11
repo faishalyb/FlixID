351e10a29c906f1d4dc4e53890850b706dcb7165
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-02-11 15:12:22 +0700

    add go router on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,31 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flix_id/firebase_options.dart';
-import 'package:flix_id/presentation/pages/login_page/login_page.dart';
+import 'package:flix_id/presentation/providers/router/router_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
   runApp(const ProviderScope(
     child: MainApp(),
-    ));
+  ));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage()
-      // Scaffold(
-      //   body: Center(
-      //     child: Text('Hello World for the first time!!!'),
-      //   ),
-      // ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: ref.watch(routerProvider).routeInformationParser,
+      routeInformationProvider:
+          ref.watch(routerProvider).routeInformationProvider,
+      routerDelegate: ref.watch(routerProvider).routerDelegate,
     );
   }
 }
--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,18 +1,26 @@
 import 'package:flix_id/domain/entities/user.dart';
+import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends ConsumerStatefulWidget {
   final User user;
   const MainPage({Key? key, required this.user}) : super(key: key);
 
   @override
+  ConsumerState<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends ConsumerState<MainPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main Page')
-        ),
+      appBar: AppBar(title: const Text('Main Page')),
       body: Center(
-        child: Text(user.toString()),
+        child: Text(ref.watch(userDataProvider).when(
+            data: (data) => data.toString(),
+            error: (error, StackTrace) => '',
+            loading: () => 'Loading')),
       ),
     );
   }
