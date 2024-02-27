c96c8b4de21af49b45c16c5a111ac9febff3b6f1
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-02-27 18:37:16 +0700

    adding bottom navbar widgets

--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flix_id/presentation/extensions/build_context_extensions.dart';
 import 'package:flix_id/presentation/providers/router/router_provider.dart';
 import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
+import 'package:flix_id/presentation/widgets/bottom_navbar.dart';
 import 'package:flix_id/presentation/widgets/bottom_navbar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -24,29 +26,47 @@ class _MainPageState extends ConsumerState<MainPage> {
     });
     return Scaffold(
       appBar: AppBar(title: const Text('Main Page')),
-      body: Center(
-        child: Column(
-          children: [
-            Text(ref.watch(userDataProvider).when(
-                data: (data) => data.toString(),
-                error: (error, StackTrace) => '',
-                loading: () => 'Loading')),
-            ElevatedButton(
-                onPressed: () {
-                  ref.read(userDataProvider.notifier).logout();
-                },
-                child: Text('Logout')),
-            const SizedBox(
-              height: 50,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                Text(ref.watch(userDataProvider).when(
+                    data: (data) => data.toString(),
+                    error: (error, StackTrace) => '',
+                    loading: () => 'Loading')),
+                ElevatedButton(
+                    onPressed: () {
+                      ref.read(userDataProvider.notifier).logout();
+                    },
+                    child: Text('Logout')),
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
             ),
-            const BottomNavbarItem(
+          ),
+          BottomNavbar(items: [
+            BottomNavbarItem(
                 index: 0,
-                isSelected: true,
+                isSelected: false,
                 title: 'Home',
-                image: 'assets/images/movie.png ',
-                selectedImage: 'assets/images/movie-selected.png')
-          ],
-        ),
+                image: 'assets/images/movie.png',
+                selectedImage: 'assets/images/movie-selected.png'),
+            BottomNavbarItem(
+                index: 0,
+                isSelected: false,
+                title: 'Ticket',
+                image: 'assets/images/ticket.png',
+                selectedImage: 'assets/images/ticket-selected.png'),
+            BottomNavbarItem(
+                index: 0,
+                isSelected: false,
+                title: 'Profile',
+                image: 'assets/images/profile.png',
+                selectedImage: 'assets/images/profile-selected.png')
+          ], onTap: (index) {}, selectedIndex: 0)
+        ],
       ),
     );
   }
