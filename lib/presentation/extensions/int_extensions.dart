5ef76ec78cb36cf5a5beb4289eed25b0179bc4d8
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-02-29 16:23:52 +0700

    solve issue currency

--- lib/presentation/extensions/int_extensions.dart
+++ lib/presentation/extensions/int_extensions.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 extension IntExtension on int {
   String toIDRCurrencyFormat() => NumberFormat.currency(
         locale: 'id_ID',
-        symbol: 'IDR',
+        symbol: 'IDR ',
         decimalDigits: 0,
       ).format(this);
 }
