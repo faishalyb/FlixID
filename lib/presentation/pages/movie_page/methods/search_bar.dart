aeeeae3a5e02e78081e87ed1efb3d40cbb4bab75
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-02-29 16:37:12 +0700

    adding search bar

--- lib/presentation/pages/movie_page/methods/search_bar.dart
+++ lib/presentation/pages/movie_page/methods/search_bar.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
 
-Widget searchBar(BuildContext context) => Container();
+Widget searchBar(BuildContext context) => Row(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width - 24 - 24 - 90,
+          height: 50,
+          margin: const EdgeInsets.only(left: 24, right: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          decoration: BoxDecoration(
+              color: Color(0XFF252836),
+              borderRadius: BorderRadius.circular(10)),
+          child: TextField(
+            style: TextStyle(color: Colors.grey.shade400),
+            decoration: const InputDecoration(
+                hintText: 'Search Movie...',
+                border: InputBorder.none,
+                icon: Icon(Icons.search)),
+          ),
+        ),
+      ],
+    );
