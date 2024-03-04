30ac99abee02df0e401fdee8334ccf212d8d7831
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-03-05 01:10:55 +0700

    add back nav bar widget movie

--- lib/presentation/pages/detail_page/detail_page.dart
+++ lib/presentation/pages/detail_page/detail_page.dart
@@ -1,13 +1,36 @@
 import 'package:flix_id/domain/entities/movie.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flix_id/presentation/pages/detail_page/methods/background.dart';
+import 'package:flix_id/presentation/providers/router/router_provider.dart';
+import 'package:flix_id/presentation/widgets/back_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends ConsumerWidget {
   final Movie movie;
 
   const DetailPage({super.key, required this.movie});
 
   @override
-  Widget build(BuildContext context) {
-    return Stack();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          ...background(),
+          ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BackNavigationBar(
+                    movie.title,
+                    onTap: () => ref.read(routerProvider).pop(),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
